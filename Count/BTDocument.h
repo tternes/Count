4346cf10824a1c37a97f5b06799a053ebb223efe
Thaddeus Ternes <t.ternes@c02j6029dkq4.hq.sococo.net>
2013-06-18 12:24:08 -0500

    Initial Commit

---
+++ Count/BTDocument.h
@@ -0,0 +1,13 @@
+//
+//  BTDocument.h
+//  Count
+//
+//  Created by Thaddeus Ternes on 6/18/13.
+//  Copyright (c) 2013 Bluetoo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface BTDocument : NSDocument
+
+@end
---
+++ Count/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Count
+//
+//  Created by Thaddeus Ternes on 6/18/13.
+//  Copyright (c) 2013 Bluetoo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
