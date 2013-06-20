8b759c3981beb67773ea0ffc1a3bdac19d5c2b57
Thaddeus Ternes <thaddeus@bluetoo.co>
2013-06-20 10:07:15 -0500

    Add support for tiling windows (Window menu)

---
+++ Count/BTCountAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  BTCountAppDelegate.h
+//  Count
+//
+//  Created by Thaddeus Ternes on 6/20/13.
+//  Copyright (c) 2013 Bluetoo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTCountAppDelegate : NSObject <NSApplicationDelegate>
+
+@end
---
+++ Count/BTCountAppDelegate.m
@@ -0,0 +1,38 @@
+//
+//  BTCountAppDelegate.m
+//  Count
+//
+//  Created by Thaddeus Ternes on 6/20/13.
+//  Copyright (c) 2013 Bluetoo. All rights reserved.
+//
+
+#import "BTCountAppDelegate.h"
+
+@implementation BTCountAppDelegate
+
+- (IBAction)tileOpenWindows:(id)sender
+{
+    // Tile across primary display
+    NSScreen *mainScreen = [NSScreen mainScreen];
+    NSRect mainFrame = mainScreen.frame;
+    
+    NSUInteger windowCount = [[NSApp windows] count];
+
+    NSInteger across = 8;
+    NSInteger down = (windowCount/across) + 1;
+
+    NSSize windowSize = NSMakeSize(mainFrame.size.width/across, mainFrame.size.height/down);
+    NSInteger x = 0;
+    NSInteger y = 0;
+    for(NSWindow *window in [NSApp windows])
+    {
+        if(x >= across) x = 0, y++;
+        
+        NSRect windowFrame = NSMakeRect(x * windowSize.width, y * windowSize.height, windowSize.width, windowSize.height);
+        [window setFrame:windowFrame display:YES];
+//        NSLog(@"Window: %i %@: %@", (int)window.windowNumber, window.title, NSStringFromRect(windowFrame));
+        x++;
+    }
+}
+
+@end
